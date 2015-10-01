090fe69fa6a69be61bd33c1dafc1d2858282fac7
David Liedle <david.liedle@gmail.com>
2015-09-30 18:22:08 -0600

    Backbone.js Data from API
    Now Backbone.js pulls data from the back end. Next up, wiring that to
    the output.

--- public/assets/js/r_main.js
+++ public/assets/js/r_main.js
@@ -2,6 +2,7 @@
  * The main file for working with RequireJS
  *
  */
+
 console.log('assets/js/r_main.js loaded!');
 
 requirejs.config({
@@ -9,7 +10,8 @@ requirejs.config({
   "baseUrl":  "./assets/lib",
   "optimize": "none",
   "shim": {
-    "bootstrap": { "deps": ["jquery"] }
+    "bootstrap": { "deps": ["jquery"] },
+    "backbone":  { "deps": ["jquery","lodash"] }
   },
   "paths": {
     "jquery": [
@@ -34,13 +36,12 @@ requirejs.config({
     ],
     "underscore": [
       "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min", // Main CDN file
-      "./underscore/underscore" // Local fallback installed by Bower
+      "./underscore/underscore-min" // Local fallback installed by Bower
     ]
   }
 
-});
+}); // End of requirejs.config()
 
-//requirejs(["r_main"]);
 require(
   [
     "jquery",
@@ -51,15 +52,80 @@ require(
   ],
   function(){
 
-  // jQuery:
-  $( function(){
+    // ======= jQuery ==============================================================================================
+    $( function(){
 
-    console.log('jQuery ready!');
+      console.log('jQuery ready!');
 
-    //$("#signature").html('test'); // Uncomment to see if jQuery has $ and can grab elements
+      //$("#signature").html('test'); // Uncomment to see if jQuery has $ and can grab elements
 
-    //$('[data-toggle="tooltip"]').tooltip(); // Was using this for hover on days with events, now using modals
+      //$('[data-toggle="tooltip"]').tooltip(); // Was using this for hover on days with events, now using modals
 
-  });
+    });
 
-});
+    // ======= Backbone.js ==========================================================================================
+
+    // Backbone.sync = function(method, model){
+    //   console.log('sync function model.url: ' + model.url)
+    // }
+
+    var Calendar = Backbone.Model.extend({
+      defaults: {
+        month: "",
+        year:  "",
+        events: []
+      }
+    });
+
+    var Months = Backbone.Model.extend({
+      initialize: function(){
+        console.log('Months model created...');
+      }
+    });
+
+    var Event  = Backbone.Model.extend({
+      defaults: {
+        id:      "",
+        title:   "",
+        time:    "",
+        month:   "",
+        day:     "",
+        year:    "",
+        address: ""
+      },
+      initialize: function(){
+        console.log('Event model created...');
+      }
+    });
+
+    var Events = Backbone.Collection.extend({
+      model: Event,
+      url:   '/api/v1/getAppointments'
+    });
+
+    var eventsCollection = new Events();
+    console.log('new Events instantiated...');
+
+    eventsCollection.fetch({
+
+      success: function(){
+        console.log( JSON.stringify(eventsCollection.models) );
+        console.log('eventsCollection.fetch() called...');
+
+        _.forEach(eventsCollection.models.events, function(event, key){
+          console.log('forEach iterating...');
+          console.log(event, key);
+        });
+
+      } // End of success
+
+    });
+
+    var months = new Months();
+
+    console.log('All done!');
+
+}); // End of require()
+
+console.log('End of r_main.js');
+
